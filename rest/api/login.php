c56adb70cf69cdb9cf592970d1d51fffa92bc57a
oravagamer <74290722+oravagamer@users.noreply.github.com>
2024-04-19 10:53:48 +0200

    Update login.php

--- rest/api/login.php
+++ rest/api/login.php
@@ -1,5 +1,6 @@
 <?php
 include_once "./conncetion.php";
+include_once "./verify_user.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $jsonData = file_get_contents('php://input');
   $data = json_decode($jsonData, true);
@@ -7,7 +8,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $data["username"];
     $password = $data["password"];
 
-    
+    if (verify_user($username, $password)) {
+      
+    }
   } else {
    http_response_code(400);
   }
