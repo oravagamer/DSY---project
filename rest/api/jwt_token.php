f5fbc4195cc9cf620ea62fd621f182d3ad601cee
oravagamer <74290722+oravagamer@users.noreply.github.com>
2024-04-19 11:07:21 +0200

    Update jwt_token.php

--- rest/api/jwt_token.php
+++ rest/api/jwt_token.php
@@ -3,6 +3,8 @@ include_once "./connection.php";
 function generate_jwt_tokens($username) {
   try {
     $connection = get_connection();
+
+    $statement = $connection->prepare('SELECT * FROM users WHERE username = ?');
   } catch (Exception $exception) {
     http_response_code(500);
   }
