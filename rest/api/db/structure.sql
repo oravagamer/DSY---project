a7f083911732be2ef8302fcc7fcb0325a8d445ed
oravagamer <ondrejknotek87@gmail.com>
2024-04-19 09:32:00 +0200

    push

--- rest/api/db/structure.sql
+++ rest/api/db/structure.sql
@@ -1,5 +1,7 @@
+CREATE DATABASE dsy_project_vroj;
+
 CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(255) UNIQUE NOT NULL,
     last_name VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
@@ -23,6 +25,55 @@ CREATE TABLE user_with_role (
 CREATE TABLE session (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
-    expired_at TIMESTAMP NOT NULL,
+    access_token VARCHAR(36) NOT NULL UNIQUE,
+    refresh_token VARCHAR(36) NOT NULL UNIQUE,
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
+
+CREATE TABLE shop_order (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    name VARCHAR(31) NOT NULL UNIQUE,
+    created_by INT NOT NULL,
+    created_for INT NOT NULL,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    finish_date TIMESTAMP NOT NULL,
+    status BOOLEAN NOT NULL,
+    description VARCHAR(255),
+    FOREIGN KEY (created_by) REFERENCES users(id),
+    FOREIGN KEY (created_for) REFERENCES users(id)
+);
+
+DELIMITER $$
+CREATE TRIGGER `generate_user_id` 
+BEFORE INSERT ON `users` FOR EACH ROW 
+BEGIN
+    SET new.id = uuid();
+END $$
+
+DELIMITER $$
+CREATE TRIGGER `generate_access_token` 
+BEFORE INSERT ON `session` FOR EACH ROW 
+BEGIN
+    SET new.access_token = uuid();
+    SET new.refresh_token = SHA2(uuid(), 256);
+END $$
+
+DELIMITER $$
+CREATE TRIGGER `generate_order_id` 
+BEFORE INSERT ON `shop_order` FOR EACH ROW 
+BEGIN
+    SET new.id = uuid();
+END $$
+
+CREATE TABLE images (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    data MEDIUMBLOB NOT NULL,
+    type VARCHAR(5) NOT NULL
+);
+
+DELIMITER $$
+CREATE TRIGGER `generate_image_id` 
+BEFORE INSERT ON `images` FOR EACH ROW 
+BEGIN
+    SET new.id = uuid();
+END $$
