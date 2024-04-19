7a86f02b84d8a606077597ead673c6e2d4a0dbbf
oravagamer <74290722+oravagamer@users.noreply.github.com>
2024-04-19 11:01:56 +0200

    Update jwt_token.php

--- rest/api/jwt_token.php
+++ rest/api/jwt_token.php
@@ -1,1 +1,9 @@
-
+<?php
+include_once "./connection.php";
+function generate_jwt_tokens($username) {
+  try {
+    $connection = get_connection();
+  } catch (Exception $exception) {
+    http_response_code(500);
+  }
+}
