c1be5a7d433fc6ef11533dcb8b9cdd1cc86c6f97
oravagamer <ondrejknotek87@gmail.com>
2024-04-22 17:55:27 +0200

    new(backend): logout finished + csrf validation add

--- rest/api/logout.php
+++ rest/api/logout.php
@@ -1,1 +1,52 @@
 <?php
+include_once "./connection.php";
+include_once "./net_funcs.php";
+include_once "./jwt_token.php";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $jsonData = file_get_contents('php://input');
+    $data = json_decode($jsonData, true);
+    if ($data !== null && $data["refresh"] !== null && $data["access"] !== null) {
+        $old_access_token_data = decrypt_jwt_token($data["access"], true);
+        $old_refresh_token_data = decrypt_jwt_token($data["refresh"], false);
+
+        if ($old_refresh_token_data["payload"]["exp"] > time() && $old_refresh_token_data["payload"]["aud"] === $old_access_token_data["payload"]["aud"]) {
+            $connection = get_connection();
+
+
+            $statement = $connection->prepare("SELECT count(*) AS count FROM session WHERE id = ? AND status = TRUE");
+
+            try {
+                $statement->execute([$old_refresh_token_data["payload"]["aud"]]);
+                $result = $statement->get_result();
+                $hash = $result->fetch_assoc();
+                $result->close();
+                $statement->close();
+
+                if ($hash["count"] !== 1) {
+                    status_exit(403);
+                }
+
+            } catch (Exception $exception) {
+                status_exit(403);
+            }
+
+            $statement = $connection->prepare("UPDATE session SET status = FALSE WHERE id = ?");
+            try {
+                $statement->execute([$old_refresh_token_data["payload"]["aud"]]);
+            } catch (Exception $exception) {
+                status_exit(500);
+            }
+
+            $statement->close();
+            $connection->close();
+
+            redirect("/");
+            status_exit(200);
+        } else {
+            status_exit(403);
+        }
+
+    } else {
+        status_exit(400);
+    }
+}
