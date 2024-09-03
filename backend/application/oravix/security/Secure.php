fa31a0b7aab066b13ee24a083aa0bdb486f3b06c
oravagamer <74290722+oravagamer@users.noreply.github.com>
2024-09-03 08:09:41 +0200

    Update Secure.php

--- backend/application/oravix/security/Secure.php
+++ backend/application/oravix/security/Secure.php
@@ -6,7 +6,14 @@ use Attribute;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 final class Secure {
-    public function __construct($role = "default" string|int) {
 
+    public function __construct($role = "default" string|int) {
+        PDO|null $connection = null;
+        try {
+            $connection = new PDO("mysql:host=" . $_ENV["settings"]["JWT_DB_SERVER"] . ";dbname=" . $_ENV["settings"]["JWT_DB_DATABASE_NAME"] . ";port=" . $_ENV["settings"]["JWT_DB_PORT"], $_ENV["settings"]["JWT_DB_USERNAME"], $_ENV["settings"]["JWT_DB_PASSWORD"]);
+        } catch (Error|Exception $e) {
+            statusExit(HttpStates::INTERNAL_SERVER_ERROR, $e->getMessage());
+        }
+        
     }
 }
