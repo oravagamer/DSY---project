d14abc0177393e279f55b04cd03b4aeebce5df0d
oravagamer <74290722+oravagamer@users.noreply.github.com>
2024-09-18 11:47:06 +0200

    Update EditUserRoles.jsx

--- frontend/src/pages/EditUserRoles.jsx
+++ frontend/src/pages/EditUserRoles.jsx
@@ -1,5 +1,46 @@
+import {useState, useEffect} from "react";
+import {
+    IconButton,
+    FormControl,
+    Input,
+    InputAdornment,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableSortLabel,
+    TableHead,
+    TablePagination,
+    TableRow,
+    Divider,
+
+} from '@mui/material';
+import {Link} from "react-router-dom";
+import useOravixFetch from "../hooks/useOravixFetch.js";
+import {backendUrl} from "../../settings.js";
+
 const EditUserRoles = () => {
-  return (<div></div>)
+  return (<TableContainer sx={{width: "100%", flexGrow: 1, height: "100%"}}>
+        <Table stickyHeader>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Level</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    data?.roles?.map && data?.roles.map(value => <TableRow key={value.id}>
+                    <TableCell>{value.name}</TableCell>
+                    <TableCell>{value.level}</TableCell>
+                    <TableCell>{value.description}</TableCell>
+                        {value.name === "admin" || value.name === "default" ? <TableCell /> : <TableCell><Link to={value.id}>Edit</Link></TableCell>}
+                    </TableRow>)
+                }
+            </TableBody>
+        </Table>
+    </TableContainer>)
 }
 
 export default EditUserRoles;
